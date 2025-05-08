c2fce622e8526ccbcce322afb5f75a34c263ea9b
Samuesl <ruess.samuel@gmail.com>
2025-05-08 18:36:26 +0200

    Speichern und runterfahren

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
 // thematische Layer
 let overlays = {
     stations: L.featureGroup(),
-    temperature: L.featureGroup().addTo(map),
+    temperature: L.featureGroup(),
+    wind: L.featureGroup().addTo(map),
 }
 
 // Layer control
@@ -28,6 +29,7 @@ L.control.layers({
 }, {
     "Wetterstationen": overlays.stations,
     "Temperatur": overlays.temperature,
+    "Windgeschwindigkeit": overlays.wind,
 }).addTo(map);
 
 // Maßstab
@@ -69,36 +71,55 @@ async function loadStations(url) {
         }
     }).addTo(overlays.stations);
     showTemperature(jsondata);
+    showWind(jsondata);
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
 
 function showTemperature(jsondata) {
     L.geoJSON(jsondata, {
-        filter: function(feature) {
-            if (feature.properties.LT > -20 && feature.properties.LT < 50){
+        filter: function (feature) {
+            if (feature.properties.LT > -20 && feature.properties.LT < 50) {
                 return true;
             }
         },
         pointToLayer: function (feature, latlng) {
-            let color = getColor (feature.properties.LT , COLORS.temperature);
+            let color = getColor(feature.properties.LT, COLORS.temperature);
             return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon",
-                    html: `<span style = "background-color:${color}">${feature.properties.LT}</span>`
+                    html: `<span style = "background-color:${color}">${feature.properties.LT.toFixed(1)}</span>`
                 }),
             })
         },
     }).addTo(overlays.temperature);
 }
-console.log(COLORS);
+
+
+
+//Winddaten
+function showWind(jsondata) {
+    L.geoJSON(jsondata, {
+        filter: function (feature) {
+            if (feature.properties.WG > 0 && feature.properties.WG < 200) {
+                return true;
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            let color = getColor(feature.properties.WG, COLORS.wind);
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    className: "aws-div-icon",
+                    html: `<span style = "background-color:${color}">${feature.properties.WG.toFixed(1)}</span>`
+                }),
+            })
+        },
+    }).addTo(overlays.wind);
+}
+
 function getColor(value, ramp) {
     for (let rule of ramp) {
         if (value >= rule.min && value < rule.max) {
             return rule.color;
         }
-    } 
+    }
 }
-
-
-let testColor = getColor( -5, COLORS.temperature);
-console.log("TestColor fuer temp -5", testColor);
